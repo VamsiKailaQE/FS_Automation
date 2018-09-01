2f2d1e15022e88e6d3039ab874f582d80483b41f
Vamsi Krishna Kaila <vamsikrishnakaila@Vamsis-MacBook-Pro.local>
2018-09-01 13:10:17 -0700

    XML

--- src/test/java/qa/fastspring/ProductServices.java
+++ src/test/java/qa/fastspring/ProductServices.java
@@ -1,22 +1,25 @@
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Base64;
+import java.nio.charset.StandardCharsets;
 
 public class ProductServices{
 
-	@Test
-	public getProducts(){
+	HttpURLConnection conn;	
+
+	@Test (groups = { "getInfo" })
+	public void getProducts(){
 
 	try{
 
-		conn.setRequestMethod("GET");
-		
+		conn.setRequestMethod("GET");		
 
 		if (conn.getResponseCode() != 200) {
 			throw new RuntimeException("Failed : HTTP error code : "
@@ -35,40 +38,37 @@ public class ProductServices{
 		}
 	}
 
-	catch (Exception e)
+	catch (Exception e) {
 	e.printStackTrace();
+
+	}
 		
 	}
 
+	@BeforeTest
+	public void getConnection(){
 
+		try {
 
-@BeforeMethod
-public getConnection(){
+			URL url = new URL("https://qa-api.fastspring.com/products");
+			conn = (HttpURLConnection) url.openConnection();
+			String username="ZRPDETN3R-YJERGUWOG95G", password="0QzCOcCGS4mgUWveTjmmTQ";
+			String encoded = Base64.getEncoder().encodeToString((username+":"+password).getBytes(StandardCharsets.UTF_8));  //Java 8
+			conn.setRequestProperty("Authorization", "Basic "+encoded);
+			conn.setRequestProperty("Accept", "application/json");
 
+		  } 
 
-	try {
+		  catch (MalformedURLException e) {
 
-		URL url = new URL("https://qa-api.fastspring.com/products");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		String username="ZRPDETN3R-YJERGUWOG95G", password="0QzCOcCGS4mgUWveTjmmTQ";
-		String encoded = Base64.getEncoder().encodeToString((username+":"+password).getBytes(StandardCharsets.UTF_8));  //Java 8
-		conn.setRequestProperty("Authorization", "Basic "+encoded);
-		conn.setRequestProperty("Accept", "application/json");
+			e.printStackTrace();
 
-	  } 
+		  } 
 
-	  catch (MalformedURLException e) {
+		   catch (IOException e) {
 
-		e.printStackTrace();
-
-	  } catch (IOException e) {
-
-		e.printStackTrace();
-
-	  }
+			 e.printStackTrace();
+		   }
+		}
 }
 
-
-
-
-}
